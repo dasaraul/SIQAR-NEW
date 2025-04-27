454e3b39845e71bbc9ef0c21d02a970adf2962f4
dasaraul <tamskun29@gmail.com>
2025-04-27 20:24:41 +0700

    initial

---
+++ siqar-backend/app/Models/Pengguna.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Pengguna extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'pengguna';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nama',
+        'email',
+        'password',
+        'peran', // admin atau karyawan
+        'status', // aktif atau nonaktif
+        'foto_profil',
+        'terakhir_login',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'terakhir_login' => 'datetime',
+    ];
+
+    public function karyawan()
+    {
+        return $this->hasOne(Karyawan::class, 'pengguna_id');
+    }
+}
